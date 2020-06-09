3dced00cd30ae46fdc9b44d87ee8e4ab02d8f4d9
CoteArthur <57949230+CoteArthur@users.noreply.github.com>
2020-06-09 19:09:50 +0200

    changed server db username and password

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -2,8 +2,8 @@ export default {
     mysql: {
         host: 'localhost',
         port: 3306,
-        user: 'root',
+        user: 'server',
         database: 'ofb',
-        password: ''
+        password: 'server'
     }
 }
