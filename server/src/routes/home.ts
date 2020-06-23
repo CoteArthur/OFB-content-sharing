8b867d63d71333f5e3350478ce3123f709474fee
CoteArthur <57949230+CoteArthur@users.noreply.github.com>
2020-06-23 11:34:18 +0200

    server routes removed trailling space

--- server/src/routes/home.ts
+++ server/src/routes/home.ts
@@ -7,9 +7,8 @@ export default class HomeRoute {
 
 		app.route('/api/login').post(homeController.login);
 		app.route('/api/selectUserInfo').post(homeController.selectUserInfo);
-		
-		app.route('/api/createUser').post(homeController.createUser);
 
+		app.route('/api/createUser').post(homeController.createUser);
 		app.route('/api/select').post(homeController.select);
 		app.route('/api/insert').post(homeController.insert);
 	}
